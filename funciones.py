d64304f041c2a3bdf0b98a63a77a38a69b4e8cd3
hansitoo23 <168017761+hansitoo23@users.noreply.github.com>
2024-06-28 16:35:05 -0400

    funciones.py

--- funciones.py
+++ funciones.py
@@ -4,24 +4,47 @@ def agregar_producto():
     print("Agregar producto")
     id = int(input("Ingrese el ID del producto"))
     nombre = input("Ingrese el nombre del producto")
-    categoria = input("Ingrese una opcion")
+    categoria = input("Ingrese una opcion  1. Electronica  2. Textil  3. Calzado")
     if categoria == 1:
         categoria = "Electronica"
     elif categoria == 2:
         categoria = "Textil"
     elif categoria == 3:
         categoria = "Calzado"
+    precio = float(input("Ingrese el precio del producto"))
+    print("Producto agregado correctamente")
     
-
-
-    with open('inventario.csv','a', newline='') as agregar_producto:
-        escribir = csv.writer(agregar_producto)
-        escribir.writerow()
-    print()
-
+    with open('inventario.csv','a', newline='') as inventario:
+        escribir = csv.writer(inventario)
+        escribir.writerow([id, nombre, categoria, precio])
+    
 def leer_inventario():
-    leer = csv.reader(agregar_producto)
-    print(agregar_producto)
+    with open('inventario.csv', 'r') as inventario:
+        leer = csv.reader(inventario)
+        for row in leer:
+            print("ID, Nombre, Categoria, Precio")
+            print(row)
 
-def Clasificar_productos():
-    nombre = 
+def clasificar_producto():
+    print("Clasificar Producto")
+    categorias = {
+        "Electronica": [],
+        "Textil": [],
+        "Calzado": []
+    }
+    with open('inventario.csv', 'r') as inventario:
+        leer = csv.reader(inventario)
+        for row in leer:
+            if row[2] == "Electronica":
+                categorias["Electronica"].append(row)
+            elif row[2] == "Textil":
+                categorias["Textil"].append(row)
+            elif row[2] == "Calzado":
+                categorias["Calzado"].append(row)
+    with open('inventario_clasificado.txt', 'w', newline='') as inventario_clasificado:
+        for categoria, productos in categorias.items():
+            inventario_clasificado.write(f"{categoria}\n")
+            for producto in productos:
+                inventario_clasificado.write(f"{producto}\n")
+            inventario_clasificado.write("\n")
+        print("Inventario clasificado correctamente")
